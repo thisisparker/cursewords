43d2aa9e1362fa1c54fda3fd2ef253b2a9a6e5e0
Parker Higgins <parker@parkerhiggins.net>
2019-01-24 23:28:46 -0500

    refactor with new cell data structure

--- cursewords.py
+++ cursewords.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import random
+import collections
 import string
 import sys
 
@@ -10,18 +10,11 @@ from blessed import Terminal
 
 from characters import *
 
-grid_x = 4
-grid_y = 2
-rows = 15
-columns = 15
 
 term = Terminal()
 
 class Cell:
-    def __init__(self, x_pos, y_pos, solution):
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-
+    def __init__(self, solution, entry=None):
         self.solution = solution
 
         self.number = None
@@ -33,71 +26,73 @@ class Cell:
     def is_letter(self):
         return self.solution in string.ascii_uppercase
 
-def main():
-    filename = sys.argv[1]
-    data = puz_load(filename)
+class Grid:
+    def __init__(self, grid_x, grid_y, puzfile):
+        self.grid_x = grid_x
+        self.grid_y = grid_y
+        self.puzfile = puzfile
 
-    print(term.enter_fullscreen())
-    print(term.clear())
+        self.row_count = 15
+        self.column_count = 15
 
-    make_grid(grid_x, grid_y)
-        
-    number_grid(data)
-    fill_grid(data)
+    def load(self):
+        self.cells = collections.OrderedDict()
+        for i in range(self.row_count):
+            for j in range(self.column_count):
+                self.cells[(j,i)] = Cell(
+                        self.puzfile.solution[i * self.row_count + j])
+        return None
 
-    ctrl = 'T'
-    with term.cbreak(), term.hidden_cursor(), term.keypad():
-        while ctrl != 'Q':
-            print(term.move(grid_y + 1, grid_x + 2) + term.reverse
-                    + ctrl.upper())
-            # row, col = term.get_location()
-            # print(term.normal + "current location is " + str(row) + ", " + str(col))
-            ctrl = term.inkey()
+    def draw(self):
+        top_row = get_top_row()
+        bottom_row = get_bottom_row()
+        middle_row = get_middle_row()
+        divider_row = get_divider_row()
 
-    print(term.exit_fullscreen())
+        print(term.move(self.grid_y, self.grid_x) + top_row)
+        for y_val in enumerate(range(self.grid_y + 1, 
+                                     self.grid_y + self.row_count * 2), 1):
+            if y_val[0] % 2 == 0:
+                print(term.move(y_val[1], self.grid_x) + divider_row)
+            else:
+                print(term.move(y_val[1], self.grid_x) + middle_row)
+        print(term.move(self.grid_y + self.row_count * 2, self.grid_x) 
+              + bottom_row)
+       
+        return None
 
-def puz_load(puzfile):
-    try:
-        p = puz.read(puzfile)
-    except:
-        sys.exit("Not a valid puzzle file.")
-
-    puzzle = []
-    for i in range(rows):
-        row = []
-        for j in range(columns):
-            row.append(Cell(i, j, p.solution[i*rows + j]))
-        puzzle.append(row)
-
-    return puzzle
-
-def number_grid(data):
-    number = 1
-    for rownum, row in enumerate(data):
-        for col, cell in enumerate(row):
+    def number(self):
+        number = 1
+        for x, y in self.cells:
+            cell = self.cells[(x,y)]
             if not cell.is_block():
-                if (cell.x_pos == 0 or cell.y_pos == 0):
+                if (x == 0 or y == 0):
                     cell.number = number
                     number += 1
-                elif (data[rownum][col - 1].is_block() or 
-                        data[rownum - 1][col].is_block()):
-                    cell.number = number
+                elif (self.cells[(x - 1, y)].is_block() or
+                        self.cells[(x, y - 1)].is_block()):
+                    self.cells[(x,y)].number = number
                     number += 1
 
-def fill_grid(data):
-    for i, row in enumerate(data):
-        row_y = grid_y + (i * 2) + 1
-        for j, cell in enumerate(row):
-            cell_x = grid_x + (j * 4) + 2
+        return None
+
+    def fill(self):
+        for x, y in self.cells:
+            y_coord = self.grid_y + (y * 2) + 1
+            x_coord = self.grid_x + (x * 4) + 2
+            cell = self.cells[(x,y)]
             if cell.is_letter():
-                print(term.move(row_y, cell_x) + cell.solution)
+                print(term.move(y_coord, x_coord) + cell.solution)
             elif cell.is_block():
-                print(term.move(row_y, cell_x - 1) + squareblock)
+                print(term.move(y_coord, x_coord - 1) + squareblock)
 
             if cell.number:
                 small = small_nums(cell.number)
-                x_pos = cell_x - 1 if len(small) == 2 else cell_x
-                print(term.move(row_y-1, x_pos), small)
+                x_pos = x_coord if len(small) == 2 else x_coord + 1
+                print(term.move(y_coord - 1, x_pos) + small)
+
+        return None
+
 
 def small_nums(number):
     small_num = ""
@@ -107,22 +102,6 @@ def small_nums(number):
         small_num += num_dict[digit]
 
     return small_num
-
-def make_grid(grid_x, grid_y):
-    top_row = get_top_row()
-    bottom_row = get_bottom_row()
-    middle_row = get_middle_row()
-    divider_row = get_divider_row()
-
-    print(term.move(grid_y, grid_x) + top_row)
-    for y_val in enumerate(range(grid_y + 1, grid_y + rows * 2), 1):
-        if y_val[0] % 2 == 0:
-            print(term.move(y_val[1], grid_x) + divider_row)
-        else:
-            print(term.move(y_val[1], grid_x) + middle_row)
-    print(term.move(grid_y + rows * 2, grid_x) + bottom_row)
-
-    return None
 
 def make_row(leftmost, middle, divider, rightmost):
     row = leftmost
@@ -143,6 +122,35 @@ def get_middle_row():
 
 def get_divider_row():
     return make_row(ltee, hline, bigplus, rtee)
+
+
+def main():
+    filename = sys.argv[1]
+    try: 
+        puzfile = puz.read(filename)
+    except:
+        sys.exit("Unable to parse {} as a .puz file.".format(filename))
+
+    print(term.enter_fullscreen())
+    print(term.clear())
+
+    grid_x = 4
+    grid_y = 2
+
+    grid = Grid(grid_x, grid_y, puzfile)
+    grid.load()
+    grid.draw()
+    grid.number()
+    grid.fill()
+
+    ctrl = 'T'
+    with term.cbreak(), term.hidden_cursor(), term.keypad():
+        while ctrl != 'Q':
+            print(term.move(grid_y + 1, grid_x + 2) + term.reverse
+                    + ctrl.upper())
+            ctrl = term.inkey()
+
+    print(term.exit_fullscreen())
 
 if __name__ == '__main__':
     main()
